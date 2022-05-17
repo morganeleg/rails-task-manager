287983f86c5724ae7831ad3fcb09fdce0b827442
morgane le gall[D <morganeleg@gmail.com>
2022-05-17 16:33:18 +0200

    2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: "tasks#index"
+
+  get "/task/:id", to: "tasks#show", as: :task
+
+  post 'tasks', to: "tasks#create"
+
+  get 'tasks/new', to: "tasks#new"
+
+  get "task/:id/edit", to: "tasks#edit", as: :edit
+
+  patch  "task/:id", to: "tasks#update"
+
+  delete "task/:id", to: "tasks#destroy"
 end
